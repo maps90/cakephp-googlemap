a65fa36833a8aba3ecb1e542172c8f2db3d860db
Raymond Lagonda <raymond.lagonda@gmail.com>
2012-06-29 20:56:10 +0700

    Code restructure for additional helper function

--- View/Helper/GoogleMapHelper.php
+++ View/Helper/GoogleMapHelper.php
@@ -49,23 +49,94 @@ class GoogleMapHelper extends AppHelper {
 		$(document).ready(function() {
 			initializeStyle();
 			initializeMap();
-			initializeMarker();
 		});
+
+EOF;
+		echo $this->Html->scriptBlock($scriptInitialize, array(
+			'inline' => false,
+		));
+
+		if ($this->useDefaultMarker) {
+			$scriptMarker =<<<EOF
+			function initializeMarker() {
+				GoogleMap.Marker.populate($this->_markers);
+			}
+			$(document).ready(function() {
+				initializeMarker();
+			});
+EOF;
+			echo $this->Html->scriptBlock($scriptMarker, array(
+				'inline' => false,
+			));
+		}
+	}
+
+	protected function _handleParameters($config, $options, $styles, $markers) {
+		if (!empty($config)) {
+			$args = array_map('strtolower', $config);
+			$this->_parseAndSaveConfig($config);
+		}
+		if (!empty($options)) {
+			$options = array_map('strtolower', $options);
+			$this->_parseAndSaveOptions($options);
+		}
+		if (!empty($styles)) {
+			$this->configureMapStyles($styles);
+		}
+		if (!empty($markers)) {
+			$this->configureMapMarkers($markers);
+		}
+	}
+
+	public function configureMapStyles($styles) {
+		$this->_styles = json_encode($styles);
+	}
+
+	public function configureMapMarkers($markers) {
+		$this->_markers = json_encode($markers);
+	}
+
+	protected function _parseAndSaveConfig($config) {
+		if (!is_array($config)) {
+			return false;
+		}
+		if (array_key_exists('container_id', $config)) {
+			$this->container = $args['container_id'];
+		}
+		$script =<<<EOF
+			GoogleMap.Map.config = {
+				container: '$this->container'
+			}
 EOF;
 		echo $this->Html->scriptBlock($script, array(
 			'inline' => false,
 		));
 	}
 
-	protected function _parseAndSaveArgs($args = array()) {
-		if (!is_array($args)) {
+	protected function _parseAndSaveOptions($options) {
+		if (!is_array($options)) {
 			return false;
 		}
-		if (array_key_exists('container_id', $args)) {
-			$this->container = $args['container_id'];
+		if (array_key_exists('zoom', $options)) {
+			$this->zoom = $options['zoom'];
 		}
-		if (array_key_exists('zoom', $args)) {
-			$this->zoom= $args['zoom'];
+		if (array_key_exists('center_lat', $options)) {
+			$this->centerLat= $options['center_lat'];
 		}
+		if (array_key_exists('center_long', $options)) {
+			$this->centerLong= $options['center_long'];
+		}
+		$script =<<<EOF
+			GoogleMap.Option.options= {
+				center: new google.maps.LatLng($this->centerLat, $this->centerLong),
+				zoom: $this->zoom,
+				mapTypeControlOptions: {
+					mapTypeIds: GoogleMap.Style.stylesIds
+				}
+			};
+EOF;
+		echo $this->Html->scriptBlock($script, array(
+			'inline' => false,
+		));
 	}
 }
